314e187ebcff8de50e86221b4ca0d674897c9af1
mouravocal <diego.moura3@gmail.com>
2022-10-21 13:30:49 -0300

    feat: Text Input stories

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,38 @@
+import { Box, ITextInputProps, TextInput, Text } from '@moura-vocal/react'
+import { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Form/Text Input',
+  component: TextInput,
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text>Email address</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<ITextInputProps>
+
+export const Primary: StoryObj<ITextInputProps> = {
+  args: {
+    placeholder: 'Type your name',
+  },
+}
+
+export const Disabled: StoryObj<ITextInputProps> = {
+  args: {
+    disabled: true,
+  },
+}
+
+export const WithPrefix: StoryObj<ITextInputProps> = {
+  args: {
+    prefix: 'cal.com/',
+  },
+}
